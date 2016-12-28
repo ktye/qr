81285cbd57e5a7df6d33dc45cf89db20bbf8ec7d
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2016-12-28 12:37:29 +0100

    add ExampleSolve

--- qr_test.go
+++ qr_test.go
@@ -35,3 +35,33 @@ func TestLeastSquareSolve(t *testing.T) {
 		}
 	}
 }
+
+func ExampleSolve() {
+	// Solution to the overdetermined 3x2 system: A x = b.
+	// Matrix A (3x2).
+	A := [][]complex128{
+		[]complex128{complex(1, 2), complex(5, -1)},
+		[]complex128{complex(6, 8), complex(1, 0)},
+		[]complex128{complex(3, -2), complex(-7, 3)},
+	}
+
+	// Right hand side (3x1):
+	b := []complex128{
+		complex(5, -2),
+		complex(-3, 1),
+		complex(3, 0),
+	}
+
+	// Compute the QR decomposition.
+	if D, err := New(A); err != nil {
+		panic(err)
+	} else {
+		// Solve the system.
+		if x, err := D.Solve(b); err != nil {
+			panic(err)
+		} else {
+			fmt.Println(x)
+		}
+	}
+	// Output: [(0.040087623220153294+0.20569550930996708i) (0.10186199342825833-0.1207009857612268i)]
+}
